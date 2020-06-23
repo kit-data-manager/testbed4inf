e9feacf844fe3796628317fabaf2cfe7919333e0
Andreas Pfeil <pfeil@dyn-dhcp-141-52-74-165.scc.kit.edu>
2020-06-23 16:56:11 +0200

    Create databases pit and collection in postgres.

--- source/postgres/docker-entrypoint-initdb.d/init.sh
+++ source/postgres/docker-entrypoint-initdb.d/init.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
-echo "Nothing to do. If required, add database initialization here."
 
 # add additional databases to the default, which is set in the compose file.
 # to add further databases and users, add the service name.
 # the service name has to fit the db name and db user name.
 # example: for service in pit metastore collectionapi; do ... done
-for service in pit;
+echo "User and DB used to create new users and dbs: user=$POSTGRES_USER, db=$POSTGRES_DB"
+for service in pit collection;
 do
+    echo "Will create user and db named $service."
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         CREATE USER $service;
         CREATE DATABASE $service;
         GRANT ALL PRIVILEGES ON DATABASE $service TO $service;
-    EOSQL
+EOSQL
 done
