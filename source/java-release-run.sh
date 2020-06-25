63001da89d2aa52c096b2f131788918cc6a21047
Andreas Pfeil <verpfeilt@scc-wlan-kit-cn-172-17-190-153.scc.kit.edu>
2020-06-25 11:12:44 +0200

    Add more logging to run script.

--- source/java-release-run.sh
+++ source/java-release-run.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -e
 
+function exec() {
+    echo "BUILD-SCRIPT COMMAND:" $@
+    $@
+    return $?
+}
+
+function log() {
+    echo "BUILD-SCRIPT LOG:" $@
+}
+
 if test $# -lt 2
 then
     echo "No arguments supplied, expected two. Call was: $@"
-    exit 1
+    exec exit 1
 fi
 
 SERVICE_FOLDER=$1
 BINARY_NAME=$2
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-function exec() {
-    echo "RUN-SCRIPT COMMAND:" $@
-    $@
-    return $?
-}
+log SERVICE_FOLDER: $SERVICE_FOLDER
+log BINARY_NAME: $BINARY_NAME
 
-cd /$SCRIPT_DIR/$SERVICE_FOLDER/release
-java -cp ".:./config:./$BINARY_NAME.jar" -Dloader.path=file://`pwd`/$BINARY_NAME.jar,./lib/,. -jar $BINARY_NAME.jar
+exec cd $SCRIPT_DIR/$SERVICE_FOLDER/release
+exec java -cp ".:./config:./$BINARY_NAME.jar" -Dloader.path=file://`pwd`/$BINARY_NAME.jar,./lib/,. -jar $BINARY_NAME.jar
