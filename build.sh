85257cfbc9743ed543ce255c4f36608cc3b67c38
Andreas Pfeil <pfeil@dyn-dhcp-141-52-74-39.scc.kit.edu>
2020-07-13 18:26:03 +0200

    Switch to multi-stage build.
    Currently only implements pit-service.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# 
+# Usage: `bash build.sh`
+# 
+# This will build the testbed containers correctly.
+# Note that you currently need an ssh key to access the repositories.
+# Your key will be copied into an intermediate container, which will
+# be deleted in the end of this script.
+
+SSH_KEY_FILE=~/.ssh/id_rsa
+
+docker-compose build --build-arg SSH_PRIVATE_KEY="$(cat $SSH_KEY_FILE)" pit-service
+
+# Delete containers with credentials.
+docker rmi -f $(docker images -q --filter label=stage=build-contains-credentials)
--- java-release-run.sh
+++ java-release-run.sh
@@ -11,18 +11,16 @@ function log() {
     echo "RUN-SCRIPT LOG:" $@
 }
 
-if test $# -lt 2
+if test $# -lt 1
 then
     echo "No arguments supplied, expected two. Call was: $@"
     exec exit 1
 fi
 
-SERVICE_FOLDER=$1
-BINARY_NAME=$2
+BINARY_NAME=$1
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-log SERVICE_FOLDER: $SERVICE_FOLDER
 log BINARY_NAME: $BINARY_NAME
 
-exec cd $SCRIPT_DIR/$SERVICE_FOLDER/release
-exec java -cp ".:./config:./$BINARY_NAME.jar" -Dloader.path=file://`pwd`/$BINARY_NAME.jar,./lib/,. -jar $BINARY_NAME.jar
+exec cd $SCRIPT_DIR
+exec java -cp ".:./config:./$BINARY_NAME.jar" -Dloader.path=file://`pwd`/$BINARY_NAME.jar,./lib/,. -jar $BINARY_NAME
