ce75089e6fc79075cae014ca0a537ab143c07e87
Andreas Pfeil <verpfeilt@Andreass-MacBook-Air.local>
2020-07-14 12:50:28 +0200

    Impl service parameters for build script.
    The build script can now take an optional parameter list of services
    it should build. If not parameter is given, all services will be built.
    Also it will output a note at the end, how the services can be started.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # 
-# Usage: `bash build.sh`
+# Usage: `bash build.sh` to build all services,
+#        `bash build.sh fairris pit-service` to build only selected services.
 # 
 # This will build the testbed containers correctly.
 # Note that you currently need an ssh key to access the repositories.
@@ -9,7 +10,14 @@
 
 SSH_KEY_FILE=~/.ssh/id_rsa
 
-docker-compose build --build-arg SSH_PRIVATE_KEY="$(cat $SSH_KEY_FILE)" fairris #pit-service
+echo "Building images."
+echo "Called script with those parameters: $@"
+
+docker-compose build --build-arg SSH_PRIVATE_KEY="$(cat $SSH_KEY_FILE)" $@
 
 # Delete containers with credentials.
 docker rmi -f $(docker images -q --filter label=stage=build-contains-credentials)
+
+echo "Finished building images."
+echo "Use the following command to start the just build images:"
+echo "docker-compose up $@"
