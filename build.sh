8746b4d44eb125fa17cee85582b11fbbb7d1b6f8
Andreas Pfeil <andreas.pfeil@kit.edu>
2020-12-14 16:57:08 +0100

    Remove the ssh credential workarounds.

--- build.sh
+++ build.sh
@@ -2,20 +2,14 @@
 # 
 # Usage: `bash build.sh` to build all services,
 #        `bash build.sh fairris pit-service` to build only selected services.
-# 
-# This will build the testbed containers correctly.
-# Note that you currently need an ssh key to access the repositories.
-# Your key will be copied into an intermediate container, which will
-# be deleted in the end of this script.
 
-SSH_KEY_FILE=~/.ssh/id_rsa
 
 echo "Building images."
 echo "Called script with those parameters: $@"
 
-docker-compose build --build-arg SSH_PRIVATE_KEY="$(cat $SSH_KEY_FILE)" $@
+docker-compose build $@
 
-# Delete containers with credentials.
+# Delete containers which may contain credentials or build artefacts.
 docker rmi -f $(docker images -q --filter label=stage=build-contains-credentials)
 
 echo "Finished building images."
